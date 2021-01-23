140b81ec7d4e9600475d97bf04433d289b8417ef
dhadk <dhadk@naver.com>
2021-01-23 18:43:54 +0900

    django course 33 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -16,17 +16,17 @@ has_ownership = [account_ownership_required, login_required]
 
 
 def hello_world(request):
-        if request.method == "POST":
-            temp = request.POST.get('hello_world_input')
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
 
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
 
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 
 class AccountCreateView(CreateView):
